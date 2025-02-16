daae98c0146123fac49615a7d7bf4a84eb3217bd
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2025-02-16 16:15:38 +0530

    feat: added validations

--- components/noob/form.tsx
+++ components/noob/form.tsx
@@ -1,16 +1,42 @@
 "use client";
 import React, { FormEvent, useState } from "react";
+import * as yup from "yup";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
+
+const SignupSchema = yup
+  .object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+  })
+  .required();
+
+type errorMessageType = yup.InferType<typeof SignupSchema>;
 
 export const NoobForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [bio, setBio] = useState("");
+  const [errors, setErrors] = useState<errorMessageType>();
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    try {
+      await SignupSchema.validate({ name, email, bio }, { abortEarly: false });
+    } catch (err) {
+      const errorObj: errorMessageType = { name: "", email: "" };
+      (err as yup.ValidationError)?.inner?.forEach((e) => {
+        if (e?.path) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          errorObj[e?.path] = e?.message;
+        }
+      });
+      setErrors(errorObj);
+      return;
+    }
+
     alert(JSON.stringify({ name, email, bio }, null, 2));
   };
 
@@ -28,6 +54,9 @@ export const NoobForm = () => {
             />
           </label>
         </div>
+        <div className="text-sm text-red-500">
+          {errors?.name && <p>{errors?.name}</p>}
+        </div>
         <div>
           <label>
             Email:
@@ -37,6 +66,9 @@ export const NoobForm = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
           </label>
+          <div className="text-sm text-red-500">
+            {errors?.email && <p>{errors?.email}</p>}
+          </div>
         </div>
         <div>
           <label>
