0be71afa718075175a96f90880378d6a19202480
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2025-02-16 18:48:15 +0530

    feat: added

--- components/base-form-simple/form.tsx
+++ components/base-form-simple/form.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import { BaseForm } from "../ui/base-form/form";
 import * as yup from "yup";
+import { FormInputType } from "../ui/base-form/form-input";
 
 const fields = [
   { name: "name", label: "Name" },
   { name: "email", label: "Email" },
+  {
+    name: "bio",
+    label: "Bio",
+    inputProps: {
+      fieldType: FormInputType.Textarea,
+    },
+  },
+  {
+    name: "gender",
+    label: "Gender",
+    inputProps: {
+      fieldType: FormInputType.Select,
+      options: [
+        {
+          label: "Male",
+          value: "male",
+        },
+        {
+          label: "Female",
+          value: "female",
+        },
+      ],
+    },
+  },
 ];
 const SignupSchema = yup
   .object({
     name: yup.string().required(),
+    bio: yup.string().required(),
+    gender: yup.string().required(),
     email: yup.string().email().required(),
   })
   .required();
